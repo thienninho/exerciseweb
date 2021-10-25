a1af2cf1f26acfcb06776df0a03a5b13892c989e
Nguyen Thien <leothien2605@gmail.com>
2021-10-25 21:18:27 +0700

    ok

---
+++ Exercise-Dashboard-master/src/java/Control/Category.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author An Le
+ */
+@WebServlet(name = "Category", urlPatterns = {"/Category"})
+public class Category extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+           String category = request.getParameter("cate");
+           if (category.equals("Member")) {
+               request.getRequestDispatcher("/Interface/Members.jsp").forward(request, response);           
+        }
+           if (category.equals("Exercise-List") || category.equals("returnDashboard")) {
+               request.getRequestDispatcher("/Interface/Exercises-List.jsp").forward(request, response);           
+        }
+        if (category.equals("Exercise4")) {
+               request.getRequestDispatcher("/Chapter4/index.html").forward(request, response);
+           }
+        if (category.equals("Exercise5")) {
+               request.getRequestDispatcher("/Chapter5/Index.html").forward(request, response);           
+        }
+        if (category.equals("Exercise7-0102")) {
+               request.getRequestDispatcher("/Chapter7-0102/index.jsp").forward(request, response);
+           }
+        if (category.equals("Exercise7-03")) {
+               request.getRequestDispatcher("/Chapter7-03/index.jsp").forward(request, response);
+           }  
+    }
+
+
+}
